b8cf22d806599ae031045154b28121fbf4df4cf9
Lenivene Bezerra <lenivene@msn.com>
2020-03-21 01:10:50 -0300

    update: service that load/go a route
    This is a good idea
    
    For example:
    ```js
    import { useHistory } from "react-router-dom";
    export default function Home() {
      let history = useHistory();
    
      const goToPage= page => {
        history.push(page);
      };
    
      return (
        <button onClick={() => goToPage("/result")}>Go to result</button>
      )
    }
    ```

--- src/src/services/history.js
+++ src/src/services/history.js
@@ -1,5 +1,5 @@
-import { createBrowserHistory } from 'history';
+import { useHistory } from "react-router-dom";
 
-const history = createBrowserHistory();
+const history = useHistory();
 
 export default history;
